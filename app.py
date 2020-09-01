5b089ac4bb7a302f5c62d0bdb2b70335f1f48d55
Maximilian Schmeling <maxicarlos08@gmail.com>
2020-09-01 08:56:17 +0200

    #2 support for multiple clients

--- app.py
+++ app.py
@@ -8,11 +8,17 @@ Created on Sat Aug 29 12:56:16 2020
 from flask import Flask, render_template, request
 import json
 import RPi.GPIO as gpio
+import os
+import glob
 gpio.setmode(gpio.BCM)
 
 
 app = Flask(__name__)
 path = '/'.join(__file__.split('/')[:-1]) + '/'
+fifo_path = '/tmp/netgpio/'
+fifo_template = fifo_path + '{:04d}.client'
+if not os.path.isdir(fifo_path):
+    os.mkdir(fifo_path)
 # %% Setup gpio varibles
 gpio_file = open(path + 'gpios.json', 'r')
 gpios = json.loads(gpio_file.read())
@@ -37,6 +43,15 @@ def processGPIOstate(gpio_dict, reverse):
     return gpio_dict
 
 
+# %% function to write fifos
+def writeFIFO():
+    fifos = glob.glob(fifo_path + '*.client')
+    for fifo in fifos:
+        with open(fifo, 'w') as fifo_file:
+            fifo_file.write('changed')
+            fifo_file.flush()
+
+
 # %% The home website
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -54,8 +69,19 @@ def home():
             new_state = request.json['state']
         pin = request.json['pin']
         gpio.output(int(pin), new_state)
-
+        writeFIFO()
         return 'ok'
+
+
+@app.route('/waitevent', methods=['POST'])
+def wait():
+    fifo_name = fifo_template.format(len(glob.glob(fifo_path + '*.client')))
+    os.mkfifo(fifo_name)
+    fifo_wait = open(fifo_name, 'r')
+    fifo_wait.read(7)
+    fifo_wait.close()
+    os.remove(fifo_name)
+    return processGPIOstate(gpios, reverse_power)
 
 
 # %% Start server if main
--- static/javascript.js
+++ static/javascript.js
@@ -13,3 +13,22 @@ function updateGPIO(gpio) {
     server.setRequestHeader('Content-Type', 'application/json');
     server.send(JSON.stringify({'pin': name, 'state': state}));
 }
+
+function updateStates(state) {
+    for (var gpio in state) {
+        document.getElementById(gpio).checked = state[gpio];
+    }
+}
+
+while (true) {
+    var server = new XMLHttpRequest();
+    server.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            updateStates(JSON.parse(server.responseText));
+        } else if (this.readyState == 4) {
+            alert('Some error on the server, Try to reload');
+        }
+    }
+    server.open('POST', '/waitevent');
+    server.send();
+}
