8ffee9eed9578e4324aedb942cef1f02c720acac
Maximilian Schmeling <maxicarlos08@gmail.com>
2020-09-01 11:33:27 +0200

    #2 added worker to prevent hang on website

---
+++ static/worker.js
@@ -0,0 +1,18 @@
+function updateStates(state) {
+    for (var gpio in state) {
+        document.getElementById(gpio).checked = state[gpio];
+    }
+}
+
+while (true) {
+    var server = new XMLHttpRequest();
+    server.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            updateStates(JSON.parse(server.responseText));
+        } else if (this.readyState == 4) {
+            alert('Some error on the server, Try to reload');
+        }
+    }
+    server.open('POST', '/waitevent');
+    server.send();
+}
